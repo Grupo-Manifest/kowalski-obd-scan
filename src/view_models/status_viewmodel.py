b245ab73d1a478e0dc739afc6891a17659e4e9d6
Kaue Miziara <kauemiziara@gmail.com>
2023-08-11 14:23:41 -0300

    Add vehicle speed info to Status ViewModel and View

--- src/view_models/status_viewmodel.py
+++ src/view_models/status_viewmodel.py
@@ -1,7 +1,15 @@
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
 from kivy.lang import Builder
+
+from scanner_connection.obd_query_handler import OBDQueryHandler
 
 Builder.load_file("views/status_view.kv")
 class StatusView(Screen):
-    pass
+    current_speed = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(StatusView, self).__init__(**kwargs)
+        obd_query = OBDQueryHandler()
+
+        self.current_speed = str(obd_query.get_speed())
