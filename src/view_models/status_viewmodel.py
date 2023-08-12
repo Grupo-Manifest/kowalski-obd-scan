44f78024d5eb830897e845d8df4fc7f58ed76eb0
Kaue Miziara <kauemiziara@gmail.com>
2023-08-12 18:28:35 -0300

    Add accelerator position info to Status ViewModel and View

--- src/view_models/status_viewmodel.py
+++ src/view_models/status_viewmodel.py
@@ -6,10 +6,12 @@ from scanner_connection.obd_query_handler import OBDQueryHandler
 
 Builder.load_file("views/status_view.kv")
 class StatusView(Screen):
-    current_speed = StringProperty()
+    current_speed        = StringProperty()
+    accelerator_position = StringProperty()
 
     def __init__(self, **kwargs):
         super(StatusView, self).__init__(**kwargs)
         obd_query = OBDQueryHandler()
 
-        self.current_speed = str(obd_query.get_speed())
+        self.current_speed        = str(obd_query.get_speed())
+        self.accelerator_position = str(obd_query.get_accelerator_position())
